ffe39cef87bfa0f0baf804cf471e2f4f0921c7f5
jolouste <jaylord.ramos.iics@ust.edu.ph>
2022-12-04 00:04:55 +0800

    modified

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,6 +4,8 @@ const instance = axios.create({
 	baseURL: "http://54.161.202.78/api",
 	headers: {
 		"Content-Type": "application/json",
+		"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+		"Access-Control-Allow-Origin:": "*",
 		Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
 	},
 });
--- src/axios/instanceNoAuth.js
+++ src/axios/instanceNoAuth.js
@@ -4,6 +4,8 @@ const instanceNoAuth = axios.create({
 	baseURL: "http://54.161.202.78/api/",
 	headers: {
 		"Content-Type": "application/json",
+		"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+		"Access-Control-Allow-Origin:": "*",
 	},
 });
 
