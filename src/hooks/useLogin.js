4a47e5ed9801efa28260bc78e82c61de0173da52
jolouste <jaylord.ramos.iics@ust.edu.ph>
2022-12-12 18:58:24 +0800

    modified

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
 		setLoading(true);
 		setError("");
 
-		const response = await fetch("http://localhost:5000/api/auth/login", {
+		const response = await fetch("http://54.86.88.74/api/auth/login", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
